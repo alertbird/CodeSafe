6ca244854d9f964df84d97b6ac5804bf79defb15
Patrick Zhang <patrick.zhang@ocado.com>
2014-11-24 17:08:04 +0000

    Initial commit for code safe. This is the parent project and settings for mercurial and git.

--- README.md
+++
@@ -1,3 +0,0 @@
-codesafe
-========
-We are going to write some code for Android platform.
---
+++ src/main/java/com/pz/codesafe/App.java
@@ -0,0 +1,13 @@
+package com.pz.codesafe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
